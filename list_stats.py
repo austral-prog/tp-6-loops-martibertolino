5defe297a27345a0cba7dfdd4074cb7e93d55d27
martibertolino <virginiacesarini195@gmail.com>
2026-05-19 19:34:18 -0300

    Update list_stats.py

--- list_stats.py
+++ list_stats.py
@@ -1,3 +1,16 @@
+def find_min(numbers):
+    minimum = numbers[0]
+    for num in numbers:
+        if num < minimum:
+            minimum = num
+    return minimum
+def find_max(numbers):
+
+    maximum = numbers[0]
+    for num in numbers:
+        if num > maximum:
+            maximum = num
+    return maximum
 def range_of(numbers):
     maximo = find_max(numbers)
     minimo = find_min(numbers)
@@ -11,15 +24,11 @@ def average(numbers):
         suma_total += num
     promedio = suma_total / len(numbers)
     return round(promedio, 1)
-
-
 def describe(numbers):
-    if not numbers:
-        return "Empty list"
     if not numbers:
         return "Empty list"
     minimo = find_min(numbers)
     maximo = find_max(numbers)
     rango = range_of(numbers)
     promedio = average(numbers)
-    return (f"Min:{minimo} Max:{maximo} Range:{rango} Avg:{promedio}")
+    return f"Min:{minimo} Max:{maximo} Range:{rango} Avg:{promedio}"
