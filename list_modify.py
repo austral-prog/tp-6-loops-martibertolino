d7801268482acc0abc4b82e19ebf913d7c57832d
martibertolino <virginiacesarini195@gmail.com>
2026-05-19 18:36:21 -0300

    Update list_modify.py

--- list_modify.py
+++ list_modify.py
@@ -1,29 +1,13 @@
-# Replace the "ANSWER HERE" for your answer
-
 def put(value, lst):
-    """
-    Coloca value en el primer lugar vacio ("") que encuentre en lst
-    y retorna el indice donde lo coloco.
-    Si no hay ningun lugar vacio, retorna -1.
-    IMPORTANTE: esta funcion modifica la lista original.
-
-    Ejemplo:
-        colors = ["Red", "", "Green"]
-        put("Blue", colors) -> 1
-        # colors ahora es ["Red", "Blue", "Green"]
-    """
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def remove(value, lst):
-    """
-    Busca todas las ocurrencias de value en lst, las reemplaza por ""
-    y retorna la cantidad de eliminaciones realizadas.
-    IMPORTANTE: esta funcion modifica la lista original.
-
-    Ejemplo:
-        colors = ["Red", "Green", "Red", "Blue"]
-        remove("Red", colors) -> 2
-        # colors ahora es ["", "Green", "", "Blue"]
-    """
-    return "ANSWER HERE"  # Remove this line and implement
+    for i in range(len(lst)):
+        if lst[i] == "":
+            lst[i] = value
+            return i
+    return -1
+def remove (value, lst):
+    count = 0
+    for i in range(len(lst)):
+        if lst[i] == value:
+            lst[i] = ""
+            count += 1
+    return count
