71c5fc9214e7ec0511f0357a1ffd046e97cb1865
martibertolino <virginiacesarini195@gmail.com>
2026-05-19 18:35:46 -0300

    Update find_extremes.py

--- find_extremes.py
+++ find_extremes.py
@@ -1,36 +1,22 @@
-# Replace the "ANSWER HERE" for your answer
-
 def find_min(numbers):
-    """
-    Dada una lista de numeros (no vacia), retorna el menor valor.
-    Usar un bucle for para recorrer la lista.
-
-    Ejemplo: find_min([3, 1, 7, 2]) -> 1
-    Ejemplo: find_min([5, 5, 5]) -> 5
-    Ejemplo: find_min([-3, -1, -7]) -> -7
-    """
-    return "ANSWER HERE"  # Remove this line and implement
-
+    min = numbers[0]
+    for num in numbers:
+        if num < min:
+            min = num
+    return min
 
 def find_max(numbers):
-    """
-    Dada una lista de numeros (no vacia), retorna el mayor valor.
-    Usar un bucle for para recorrer la lista.
-
-    Ejemplo: find_max([3, 1, 7, 2]) -> 7
-    Ejemplo: find_max([5, 5, 5]) -> 5
-    Ejemplo: find_max([-3, -1, -7]) -> -1
-    """
-    return "ANSWER HERE"  # Remove this line and implement
-
+    max = numbers[0]
+    for num in numbers:
+        if num > max:
+            max = num
+    return max
 
 def count_negatives(numbers):
-    """
-    Dada una lista de numeros, retorna la cantidad de numeros negativos.
-    Si la lista esta vacia, retorna 0.
-
-    Ejemplo: count_negatives([3, -1, -7, 2]) -> 2
-    Ejemplo: count_negatives([1, 2, 3]) -> 0
-    Ejemplo: count_negatives([-1, -2, -3]) -> 3
-    """
-    return "ANSWER HERE"  # Remove this line and implement
+    if not numbers:
+        return 0
+    contador = 0
+    for num in numbers:
+        if num < 0:
+           contador += 1
+    return contador
