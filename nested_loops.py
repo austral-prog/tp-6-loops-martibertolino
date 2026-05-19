e7983af3cb8a50dfb44f0a3407d7bb0edb84ee3e
martibertolino <virginiacesarini195@gmail.com>
2026-05-19 18:38:12 -0300

    Update nested_loops.py

--- nested_loops.py
+++ nested_loops.py
@@ -1,31 +1,25 @@
-# Replace the "ANSWER HERE" for your answer
-
 def flatten(matrix):
-    """
-    Dada una lista de listas (matriz), retorna una unica lista
-    con todos los elementos en orden.
-
-    Ejemplo: flatten([[1, 2], [3, 4], [5, 6]]) -> [1, 2, 3, 4, 5, 6]
-    """
-    return "ANSWER HERE"  # Remove this line and implement
-
+    lista = []
+    for fila in matrix:
+        for elemento in fila:
+            lista.append(elemento)
+    return lista
 
 def row_sums(matrix):
-    """
-    Dada una matriz (lista de listas de numeros), retorna una lista
-    donde cada elemento es la suma de la fila correspondiente.
-
-    Ejemplo: row_sums([[1, 2, 3], [4, 5, 6]]) -> [6, 15]
-    """
-    return "ANSWER HERE"  # Remove this line and implement
+    answer = []
+    for fila in matrix:
+        answer.append(sum(fila))
+    return answer
 
 
 def col_sums(matrix):
-    """
-    Dada una matriz (lista de listas de numeros), retorna una lista
-    donde cada elemento es la suma de la columna correspondiente.
-    Se asume que todas las filas tienen la misma longitud.
-
-    Ejemplo: col_sums([[1, 2, 3], [4, 5, 6]]) -> [5, 7, 9]
-    """
-    return "ANSWER HERE"  # Remove this line and implement
+    if len (matrix) == 0:
+        return []
+    num_colms = len(matrix[0])
+    result = []
+    for col in range(num_colms):
+        total = 0
+        for fila in matrix:
+            total = total + fila[col]
+        result.append(total)
+    return result
